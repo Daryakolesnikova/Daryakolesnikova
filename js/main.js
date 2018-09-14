3d007ea91e8672e6dbfae35c4ee05d980b4bb200
Daryakolesnikova <d@bvz.name>
2018-09-14 16:18:39 +0300

    настроила открытие картинок wip

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
-var openPhotoSwipe = function() {
-
-    var pswpElement = document.querySelectorAll('.pswp')[0];
-
-    // build items array
-    var items = [
-        {
-            src: 'https://placekitten.com/600/400',
-            w: 600,
-            h: 400
-        },
-        {
-            src: 'https://placekitten.com/1200/900',
-            w: 1200,
-            h: 900
-        }
-    ];
-
-    // define options (if needed)
-    var options = {
-        // optionName: 'option value'
-        // for example:
-        index: 0 // start at first slide
-    };
-
-    // Initializes and opens PhotoSwipe
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-    gallery.init();
-};
-
 window.addEventListener('DOMContentLoaded', function () {
-    document.getElementsByTagName('img')[0].onclick = openPhotoSwipe;
+    var imageElements = document.querySelectorAll('.pic > img');
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+    var images = Array.prototype.slice.call(imageElements);
+
+    images.forEach(function (image) {
+        image.onload = function() {
+            image.data = {
+                src: image.src,
+                w: image.naturalWidth,
+                h: image.naturalHeight
+            };
+        };
+
+        image.onclick = function () {
+            var items = images.map(function (image) {
+                return image.data;
+            });
+
+            var index = items.findIndex(function (item) {
+                return item.src === image.src;
+            });
+
+            var options = {
+                index: index
+            };
+
+            var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+
+            gallery.init();
+        };
+    });
 });
